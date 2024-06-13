1f7f111818fb34f7481d7c76f2dfe08fc4683a59
Hao Wang <whwanghao027@gmail.com>
2024-06-13 16:44:18 -0400

    Spring Initialize

---
+++ src/main/java/com/hao/ratemycharacter/RatemycharacterApplication.java
@@ -0,0 +1,13 @@
+package com.hao.ratemycharacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatemycharacterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatemycharacterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hao/ratemycharacter/RatemycharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hao.ratemycharacter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatemycharacterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
